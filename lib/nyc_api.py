779c69127112e99def9ca9a25b6616f7a612a808
Will Chang <william.chaang@gmail.com>
2023-08-14 05:04:06 -0600

    API pulled. So cool!

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -9,7 +9,21 @@ class GetPrograms:
 
     response = requests.get(URL)
     return response.content
+  
+  def program_school(self):
+      # we use the JSON library to parse the API response into nicely formatted JSON
+      programs_list = []
+      programs = json.loads(self.get_programs())
+      for program in programs: 
+         programs_list.append(program["agency"])
+        
+      return programs_list
+  
 
+# programs = GetPrograms().get_programs()
+# print(programs)
+programs = GetPrograms()
+programs_schools = programs.program_school()
 
-programs = GetPrograms().get_programs()
-print(programs)
+for school in set(programs_schools):
+   print(school)
